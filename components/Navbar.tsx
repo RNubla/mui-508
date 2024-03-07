c89fbcf285915b3d021badc519826e7c879376a3
RNubla <rnubla97@gmail.com>
2024-03-07 07:05:44 -0500

    fix: formatting

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,46 +1,46 @@
 "use client";
 import * as React from "react";
 import {
-  AppBar,
-  Box,
-  Button,
-  Container,
-  IconButton,
-  Toolbar,
-  Typography,
+	AppBar,
+	Box,
+	Button,
+	Container,
+	IconButton,
+	Toolbar,
+	Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
 interface NavBarProps {
-  children?: React.ReactNode;
+	children?: React.ReactNode;
 }
 
 export const NavBar: React.FC<NavBarProps> = ({ children }) => {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Brand Name
-          </Typography>
-          <Button color="inherit">Notification</Button>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Theme</Button>
-        </Toolbar>
-      </AppBar>
+	return (
+		<Box sx={{ flexGrow: 1 }}>
+			<AppBar position="static">
+				<Toolbar>
+					<IconButton
+						size="large"
+						edge="start"
+						color="inherit"
+						aria-label="menu"
+						sx={{ mr: 2 }}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+						Brand Name
+					</Typography>
+					<Button color="inherit">Notification</Button>
+					<Button color="inherit">Login</Button>
+					<Button color="inherit">Theme</Button>
+				</Toolbar>
+			</AppBar>
 
-      <Box component={"main"} sx={{ p: 3 }}>
-        {children}
-      </Box>
-    </Box>
-  );
+			<Box component={"main"} sx={{ p: 3 }}>
+				{children}
+			</Box>
+		</Box>
+	);
 };
