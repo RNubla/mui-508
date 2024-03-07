370426ee0ec9ee0f1de80a6f6347be17075b06f4
Robert Nubla <rnubla97@gmail.com>
2024-03-06 22:47:14 -0500

    fix: MenuIcon is imported correctly

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,30 +1,31 @@
 "use client";
 import * as React from "react";
 import { AppBar, Box, Container, IconButton, Toolbar } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 interface NavBarProps {
-	children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 export const NavBar: React.FC<NavBarProps> = ({ children }) => {
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Container>
-					<Toolbar disableGutters>
-						<IconButton
-							size="large"
-							edge="start"
-							color="inherit"
-							aria-label="menu"
-							sx={{ mr: 2 }}
-						>
-							<MenuIcon />
-						</IconButton>
-					</Toolbar>
-				</Container>
-			</AppBar>
-			{children}
-		</Box>
-	);
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Container>
+          <Toolbar disableGutters>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      {children}
+    </Box>
+  );
 };
