2e7640814d06371852a782b6661faaf5fde18c97
Robert Nubla <rnubla97@gmail.com>
2024-03-06 23:32:51 -0500

    add menu buttons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,14 @@
 "use client";
 import * as React from "react";
-import { AppBar, Box, Container, IconButton, Toolbar } from "@mui/material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
 interface NavBarProps {
@@ -11,21 +19,28 @@ export const NavBar: React.FC<NavBarProps> = ({ children }) => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Container>
-          <Toolbar disableGutters>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-        </Container>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Brand Name
+          </Typography>
+          <Button color="inherit">Notification</Button>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Theme</Button>
+        </Toolbar>
       </AppBar>
-      {children}
+
+      <Box component={"main"} sx={{ p: 3 }}>
+        {children}
+      </Box>
     </Box>
   );
 };
