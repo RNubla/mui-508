456d2e6f6cfd275ee4952d172d76dae35ee77a3b
RNubla <rnubla97@gmail.com>
2024-03-12 08:43:05 -0400

    feat: add types for form

--- components/Form/types.ts
+++ components/Form/types.ts
@@ -20,18 +20,27 @@ interface InputFieldProps extends ControllerBase, TextFieldProps {
     helperText: InputField['helperText'];
 }
 // Define a generic type alias for the array schema
-type ArraySchema<T> = z.ZodType<T[], any>;
+type ArraySchema<T> = z.ZodType<T[], unknown>;
 
 const SelectFieldSchema = z.object({
     label: z.string()
 })
 type SelectField = z.infer<typeof SelectFieldSchema>
 
-interface SelectFieldProps extends SelectField, ControllerBase, AutocompleteProps 
+const KeyValuePairSchema = z.object({
+    key: z.string(),
+    value: z.unknown()
+})
+
+type KeyValuePair<K extends string, V> = z.infer<typeof KeyValuePairSchema>
+
+interface SelectFieldProps extends SelectField, ControllerBase {
+    options: KeyValuePair<string, unknown>[]
+}
 
 
 
-	export type {
+export type {
     InputFieldProps,
     SelectFieldProps, ArraySchema
 };
