94fd10b3a72c09e2e24e667f4bb286765185e979
RNubla <rnubla97@gmail.com>
2024-03-22 08:48:06 -0400

    feat: implemented custom style for text input

--- app/theme.ts
+++ app/theme.ts
@@ -2,6 +2,7 @@
 'use client';
 import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
+import { colors } from '@mui/material';
 
 const roboto = Roboto({
     weight: ['300', '400', '500', '700'],
@@ -17,7 +18,33 @@ const theme = createTheme({
                     boxShadow: 'none'
                 }
             }
+        },
+        MuiTextField: {
+            defaultProps: {
+                size: "small",
+                InputLabelProps: { shrink: true },
+                InputProps: { notched: false }
+            },
+            styleOverrides: {
+                root: {
+                    "& .MuiInputLabel-root": {
+                        transform: 'none',
+                        position: "static",
+                        marginBottom: 5,
+                        marginTop: 5
+                    },
+                    '& .MuiOutlinedInput-root': {
+                        '& fieldset': {
+                            borderWidth: '1px', // Increase the border width
+                        },
+                        '&.Mui-focused fieldset': {
+                            borderWidth: '5px', // Increase the border width when focused
+                        },
+                    },
+                }
+            }
         }
+
     },
     typography: {
         fontFamily: roboto.style.fontFamily,
